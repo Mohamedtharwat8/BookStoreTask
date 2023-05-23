8974caa68e12032c4ffa48bfae5a7459e26ca682
Mohamedtharwat8 <m.tharwat897@gmail.com>
2023-05-23 15:50:36 +0300

    Add project files.

---
+++ BookStoreTask.Application/Book/Commands/CreateBook/CreateBookCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using BookStoreTask.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStoreTask.Application.Interfaces;
+
+namespace BookStoreTask.Application.Book.Commands.CreateBook
+{
+    public class CreateBookCommandHandler : IRequestHandler<CreateBookCommand, int>
+    {
+        private readonly IBookStoreDbContext _dbContext;
+        public CreateBookCommandHandler(IBookStoreDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<int> Handle(CreateBookCommand request,
+            CancellationToken cancellationToken)
+        {
+            var book = new Book()
+            {
+                Title = request.Title,
+                Genre = request.Genre,
+                Author = request.Author,
+                Price = request.Price,
+                Amount = request.Amount,
+                CreationYear = request.CreationYear,
+            };
+
+            await _dbContext.Books.AddAsync(book, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return book.Id;
+        }
+    }
+}
---
+++ BookStoreTask.Application/Interfaces/IBookStoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookStoreTask.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreTask.Application.Interfaces
+{
+    public interface IBookStoreDbContext
+    {
+        DbSet<Book> Books { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
